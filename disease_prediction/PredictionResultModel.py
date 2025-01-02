4fe9ca812745dc667e253daa48e3f45426ef2bc8
sonms <smsms5676@naver.com>
2025-01-02 22:02:00 +0900

    chore/중요 피처 변경 후 모델 재학습

--- disease_prediction/PredictionResultModel.py
+++ disease_prediction/PredictionResultModel.py
@@ -8,12 +8,19 @@ def load_model():
     """
     학습된 모델과 중요 피처 리스트를 로드하는 함수
     """
-    # 중요 피처 정의
     important_features = [
-        "fatigue", "vomiting", "high_fever", "loss_of_appetite", "nausea",
-        "headache", "abdominal_pain", "yellowish_skin", "yellowing_of_eyes",
-        "chills", "skin_rash", "malaise", "chest_pain", "joint_pain", "sweating"
-    ]
+            "itching", "joint_pain", "stomach_pain", "vomiting", "fatigue",
+            "high_fever", "dark_urine", "nausea", "loss_of_appetite",
+            "abdominal_pain", "diarrhoea", "mild_fever", "yellowing_of_eyes",
+            "chest_pain", "muscle_weakness", "muscle_pain", "altered_sensorium",
+            "family_history", "mucoid_sputum", "lack_of_concentration"
+        ]
+    # 중요 피처 정의
+    # important_features = [
+    #     "fatigue", "vomiting", "high_fever", "loss_of_appetite", "nausea",
+    #     "headache", "abdominal_pain", "yellowish_skin", "yellowing_of_eyes",
+    #     "chills", "skin_rash", "malaise", "chest_pain", "joint_pain", "sweating"
+    # ]
 
     # 현재 파일의 경로
     current_file = Path(__file__).resolve()
